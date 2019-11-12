443c19fe0752e6e20497dc58d882e5a6ef5c60d1
Ethan Miller <ethanzanemiller@gmail.com>
2019-11-11 21:51:26 -0500

    Update 1.9 answer

--- ch1/1-9.rkt
+++ ch1/1-9.rkt
@@ -8,21 +8,31 @@
     b
     (inc (+ (dec a) b))))
 
-(+ 2 3)
-(inc (+ (dec 2) 3))
-(inc (inc (+ (dec 1) 3)))
-;; (inc (inc 3))
-;; (inc 4)
+;; (+ 4 5)
+;; (inc (+ 3 5))
+;; (inc (inc (+ 2 5)))
+;; (inc (inc (inc (+ 1 5))))
+;; (inc (inc (inc (inc (+ 0 5)))))
+;; (inc (inc (inc (inc 5))))
+;; (inc (inc (inc 6)))
+;; (inc (inc 7))
+;; (inc 8)
+;; 9
 
-;; ;; Fn B
-;; (define (+ a b)
-;;   (if (= a 0)
-;;     b
-;;     (+ (dec a) (inc b))))
+;; Fn B
+(define (+ a b)
+  (if (= a 0)
+    b
+    (+ (dec a) (inc b))))
 
-;; (+ 2 3)
-;; (+ (dec 2) (inc 3))
-;; (+ (dec 1) (inc 4))
-;; 5
+;; (+ 4 5)
+;; (+ 3 6)
+;; (+ 2 7)
+;; (+ 1 8)
+;; (+ 0 9)
+;; 9
+
 
 ; Fn A is recursive; B is iterative.
+
+
